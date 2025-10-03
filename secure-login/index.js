ed5c18ed3738047beb88361a016d5cff45e40de7
piere1234 <pierre.lemancq35@gmail.com>
2025-10-03 20:20:37 +0200

    Corrected strucutue and routing, hid plain sight files and created proper API

--- secure-login/index.js
+++ secure-login/index.js
@@ -2,6 +2,15 @@ console.log("Connecting to DB...");
 require("dotenv").config();
 console.log("DB URL:", process.env.DATABASE_URL);
 
+const path = require("path");
+
+function requireAuth(req, res, next) {
+  if (!req.session.userId) return res.status(401).send("Oh oh! It seems you are not logged in.");
+  next();
+}
+
+
+
 const express = require("express");
 const session = require("express-session");
 const pg = require("pg");
@@ -9,8 +18,30 @@ const bcrypt = require("bcryptjs");
 const pgSession = require("connect-pg-simple")(session);
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+function getcookie(req) {
+    var cookies = req.headers.cookie;
+    if (cookies) {
+        var list = {}; 
+        cookies.split(';').forEach(function(cookie) {
+            var parts = cookie.split('=');
+            list[parts.shift().trim()] = decodeURI(parts.join('='));
+        });
+        return list;
+    }
+    return null;
+}
+
+function noCache(req, res, next) {
+  res.set("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+  res.set("Pragma", "no-cache");
+  res.set("Expires", "0");
+  res.set("Surrogate-Control", "no-store");
+  next();
+}
 
 // PostgreSQL pool
 const pool = new pg.Pool({
@@ -27,15 +58,34 @@ app.use(
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30 days
+    cookie: { maxAge: 1 * 1 * 60 * 60 * 1000 }, // 1 hour
   })
 );
 
-app.use(express.static("public"));
+app.use(express.static("assets"));
 
 
 // Signup route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/signup", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "signup.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "login.html"));
+});
+
+app.get("/about_us", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "about_us.html"));
+});
+
+
+
 app.post("/signup", async (req, res) => {
+  
   const { username, password } = req.body;
 
   const hashedPassword = await bcrypt.hash(password, 10);
@@ -52,7 +102,7 @@ app.post("/signup", async (req, res) => {
 });
 
 // Login route
-app.post("/login", async (req, res) => {
+app.post("/login", noCache, async (req, res) => {
   const { username, password } = req.body;
 
   const result = await pool.query("SELECT * FROM users WHERE username=$1", [
@@ -68,21 +118,19 @@ app.post("/login", async (req, res) => {
 
   req.session.userId = user.id;
   res.send("Logged in successfully!");
-});
+  });
 
 // Protected route
-app.get("/dashboard", (req, res) => {
-  if (!req.session.userId) {
-    return res.status(401).send("Not authorized");
-  }
-  res.send("Welcome to your dashboard!");
+app.get("/dashboard", requireAuth, noCache, (req, res) => {
+  res.sendFile(path.join(__dirname, "protected", "dashboard.html"));
 });
 
 // Logout
-app.post("/logout", (req, res) => {
-  req.session.destroy();
-  res.send("Logged out.");
+app.post("/logout", (req, res, next) => {
+  res.clearCookie('connect.sid');
+  res.send("Logged out");  
 });
+
 
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
